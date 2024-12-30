249201c4b80a20bb3aacabe66d6be3cf2bf07869
Maheen <161057416+Maheen2024@users.noreply.github.com>
2024-12-30 17:41:14 +0500

    Update page.tsx

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+// 'use client';
 import React from 'react';
 import CommentSection from '@/components/CommentSection';
 import AuthorCard from '@/components/AuthorCard';
@@ -72,19 +72,19 @@ const posts = [
 
 
 
-export default function Post({ params }: { params: { id: string } }) {
-  const { id } = params;
+export default async function Post({ params }: { params: Promise<{ id: string }> }) {
+  const resolvedParams = await params;
+  const { id } = resolvedParams;
   const post = posts.find((p) => p.id === id);
 
   if (!post) {
     return (
       <h2 className='text-2xl font-bold text-center mt-10'>Post not found</h2>
-
     );
   }
 
   const renderParagraphs = (description: string) => {
-    return description.split('/n').map((para, index) => (
+    return description.split('\n').map((para, index) => (
       <p key={index} className='mt-4 text-justify'>{para.trim()}</p>
     ));
   };
@@ -94,24 +94,22 @@ export default function Post({ params }: { params: { id: string } }) {
       <h1 className='md:text-4xl text-3xl font-bold text-red-600 text-center'>
         {post.title}
       </h1>
-      <h1 className='md:text-4xl text-3xl font-bold text-red-600 text-center'>
-        {post.id}
-      </h1>
 
       {post.image && (
         <img
           src={post.image}
           alt={post.title}
-          className='w-full h-auto rounded-md mt-4' />
-
+          className='w-full h-auto rounded-md mt-4'
+        />
       )}
+
       <div className='mt-6 text-lg text-slate-700'>
         {renderParagraphs(post.description)}
-
       </div>
+
       <CommentSection postId={post.id} />
       <AuthorCard />
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
